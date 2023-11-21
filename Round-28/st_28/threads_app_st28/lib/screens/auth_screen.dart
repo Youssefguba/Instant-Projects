c7aec68482f713a2c43d4460484dd126e6cf2a14
Youssefguba <Youssef.guba@abwaab.me>
2023-11-22 01:51:16 +0200

    Push Threads App

--- Round-28/st_28/threads_app_st28/lib/screens/auth_screen.dart
+++ Round-28/st_28/threads_app_st28/lib/screens/auth_screen.dart
@@ -43,7 +43,9 @@ class AuthScreen extends StatelessWidget {
 
   Future<UserCredential> signInWithGoogle() async {
     // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+      scopes: ['profile', 'email'],
+    ).signIn();
 
     // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
